1ca4324f9209f93a53684bf6df9a378d3ae8cd57
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-04 09:53:16 +1000

    added in form template adjusted main page alignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Space, Row, Col, Divider } from 'antd';
 import './App.css';
 import Page from './components/chart';
 import MainMenu from './components/menu';
-import { Row, Col, Divider } from 'antd';
 import DemoWaterfall from './components/waterfall';
 import DemoPie from './components/pie';
+import FormLayoutDemo from './components/inputForm';
 
 const onCLickHandler = () => {
   console.log("test")
@@ -14,34 +14,34 @@ const onCLickHandler = () => {
 const App = () => (
   <div className="App">
     <MainMenu></MainMenu>
-    <Row>
-    <Col span={6}>
-
-
-      
-
-    </Col>
+    <Row justify="space-around" align="middle">
+      <Col span={6}>
+        <Row justify="space-around" align="middle">
+          <Col>
+            <FormLayoutDemo></FormLayoutDemo>
+          </Col>
+        </Row>
+      </Col>
       <Col span={18}>
-    <Row>
-      <Col offset={1} span={22}>
-        <DemoWaterfall></DemoWaterfall>
+        <Row>
+          <Col offset={1} span={22}>
+            <DemoWaterfall></DemoWaterfall>
+          </Col>
+        </Row>
+        <Row>
+          <Col offset={1} span={22}>
+            <Divider />
+          </Col>
+        </Row>
+        <Row>
+          <Col offset={1} span={10}>
+            <DemoPie></DemoPie>
+          </Col>
+          <Col offset={2} span={10}>
+            <DemoPie></DemoPie>
+          </Col>
+        </Row>
       </Col>
-    </Row>
-    <Row>
-      <Col offset={1} span={22}>
-        <Divider />
-      </Col>
-    </Row>
-
-    <Row>
-      <Col offset={1} span={10}>
-        <DemoPie></DemoPie>
-      </Col>
-      <Col offset={2} span={10}>
-        <DemoPie></DemoPie>
-      </Col>
-    </Row>
-    </Col>
     </Row>
     {/* <Button type="primary" onClick={onCLickHandler}>Button</Button> */}
   </div>
