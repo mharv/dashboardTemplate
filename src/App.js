7c33898cc69b136cf4e149370b704b836d5db45f
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-04 10:12:23 +1000

    implement layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Space, Row, Col, Divider } from 'antd';
+import { Button, Space, Row, Col, Divider, Layout } from 'antd';
 import './App.css';
 import Page from './components/chart';
 import MainMenu from './components/menu';
@@ -7,13 +7,18 @@ import DemoWaterfall from './components/waterfall';
 import DemoPie from './components/pie';
 import FormLayoutDemo from './components/inputForm';
 
+const { Header, Content, Footer } = Layout;
+
 const onCLickHandler = () => {
   console.log("test")
 }
 
 const App = () => (
   <div className="App">
-    <MainMenu></MainMenu>
+    <Layout>
+      <MainMenu></MainMenu>
+      
+      <Content>
     <Row justify="space-around" align="middle">
       <Col span={6}>
         <Row justify="space-around" align="middle">
@@ -43,7 +48,12 @@ const App = () => (
         </Row>
       </Col>
     </Row>
+    </Content>
+    <Footer> 
+        test footer - 2021
+    </Footer>
     {/* <Button type="primary" onClick={onCLickHandler}>Button</Button> */}
+    </Layout>
   </div>
 );
 
