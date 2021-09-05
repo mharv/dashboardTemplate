72fa2dcb1f1417f1e6f265403777da5aed63fc7d
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-05 22:53:07 +1000

    simplify menu items

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { MailOutlined, DotChartOutlined, SettingOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 
@@ -26,26 +26,14 @@ class MainMenu extends React.Component {
     const { current } = this.state;
     return (
       <Menu style={centerStyle} theme="light" onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
+        <Menu.Item key="home" icon={<MailOutlined />}>
+          Home
         </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
+        <Menu.Item key="compare" icon={<DotChartOutlined />}>
+          Compare
         </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
+        <Menu.Item key="admin" icon={<SettingOutlined />}>
+          Admin
         </Menu.Item>
       </Menu>
     );
