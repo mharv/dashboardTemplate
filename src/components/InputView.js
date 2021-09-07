11dd1197889db52701a09f4acc4c0a75571a634d
mitchell-harvey <mitchell.harvey@arup.com>
2021-09-07 16:45:20 +1000

    seperate input view into own component

---
+++ src/components/InputView.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Button, Space, Row, Col, Divider, Layout, Card } from 'antd';
+import DemoWaterfall from './waterfall';
+import EmbodiedCarbonPie from './EmbodiedCarbonPie';
+import EnergyCarbonPie from './EnergyCarbonPie';
+import InputTabs from './tabs';
+import TotalsTable from './TotalsTable';
+
+const { Header, Content, Footer } = Layout;
+
+const onCLickHandler = () => {
+    console.log("test")
+  }
+  
+  const centerStyle = {
+    // position: 'relative',
+    display: 'flex',
+    justifyContent: 'center'
+  };
+
+const InputView = () => (
+    <>
+      
+          <Row>
+            <Col span={8}>
+              <Row justify="space-around" align="top">
+                <Col flex='auto'>
+                  <InputTabs></InputTabs>
+                </Col>
+              </Row>
+              {/* <Row>
+                <Col offset={1} span={22}>
+                  <Divider />
+                </Col>
+              </Row> */}
+              <Row justify="space-between" align="bottom">
+                <Col flex='auto'>
+                  <TotalsTable></TotalsTable>
+                </Col>
+              </Row>
+            </Col>
+            <Col span={16}>
+              <Row>
+                <Col offset={1} span={22}>
+                  <DemoWaterfall></DemoWaterfall>
+                </Col>
+              </Row>
+              <Row>
+                <Col offset={1} span={22}>
+                  <Divider />
+                </Col>
+              </Row>
+              <Row>
+                <Col offset={1} span={10}>
+                  <EmbodiedCarbonPie></EmbodiedCarbonPie>
+                </Col>
+                <Col offset={2} span={10}>
+                  <EnergyCarbonPie></EnergyCarbonPie>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        
+    </>
+  );
+  
+  export default InputView;
