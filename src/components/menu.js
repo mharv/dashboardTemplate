aad5608938f5eb795f77b3e480b4b61bde162367
mitchell-harvey <mitchell.harvey@arup.com>
2021-09-21 14:35:06 +1000

    update menu icons and active status for links

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, DotChartOutlined, SettingOutlined, EditOutlined } from '@ant-design/icons';
+import { HomeOutlined, MailOutlined, DotChartOutlined, SettingOutlined, EditOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom"
 
 const centerStyle = {
@@ -26,7 +26,7 @@ class MainMenu extends React.Component {
     return (
       <Menu style={centerStyle} theme="light" onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
         
-        <Menu.Item key="home" icon={<MailOutlined />}>
+        <Menu.Item key="home" icon={<HomeOutlined />}>
           
         <Link to="/">
           Home 
@@ -34,17 +34,17 @@ class MainMenu extends React.Component {
         </Menu.Item> 
         <Menu.Item key="NZCinput" icon={<EditOutlined />}>
           
-        <Link to="/input">
+        <Link to="/NZCinput">
           NZC Buildings
         </Link>
         </Menu.Item>
-        <Menu.Item key="KPIinput" icon={<EditOutlined />}>
+        <Menu.Item disabled={true} key="KPIinput" icon={<EditOutlined />}>
           
-        <Link to="/input">
+        <Link to="/KPIinput">
           KPIs
         </Link>
         </Menu.Item>
-        <Menu.Item key="compare" icon={<DotChartOutlined />}>
+        <Menu.Item disabled={true} key="compare" icon={<DotChartOutlined />}>
         <Link to="/compare">
           Compare
           </Link>
