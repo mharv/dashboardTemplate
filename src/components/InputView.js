cd46a1e6e3fc30fe5b22196b4923593fe75f5db1
mitchell-harvey <mitchell.harvey@arup.com>
2021-09-21 10:56:01 +1000

    rework inputview layout

--- src/components/InputView.js
+++ src/components/InputView.js
@@ -9,49 +9,27 @@ import TotalsTable from './TotalsTable';
 
 const InputView = () => (
     <>
-      
-          <Row>
-            <Col span={8}>
-            <Space direction="vertical" size={150}>
-              <Row justify="space-around" align="top">
-                <Col flex='auto'>
-                  <InputTabs></InputTabs>
-                </Col>
-              </Row>
-              {/* <Row>
-                <Col offset={1} span={22}>
-                  <Divider />
-                </Col>
-              </Row> */}
-              <Row justify="space-between" align="bottom">
-                <Col flex='auto'>
-                  <TotalsTable></TotalsTable>
-                </Col>
-              </Row>
-              </Space>
-            </Col>
-            <Col span={16}>
-              <Row>
-                <Col offset={1} span={22}>
-                  <DemoWaterfall></DemoWaterfall>
-                </Col>
-              </Row>
-              <Row>
-                <Col offset={1} span={22}>
-                  <Divider />
-                </Col>
-              </Row>
-              <Row>
-                <Col offset={2} span={10}>
-                  <EmbodiedCarbonPie></EmbodiedCarbonPie>
-                </Col>
-                <Col offset={2} span={10}>
-                  <EnergyCarbonPie></EnergyCarbonPie>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        
+
+    <Row>
+      <Col span={8}>
+      <InputTabs></InputTabs>
+      </Col>
+      <Col offset={1} span={15}>
+        <DemoWaterfall></DemoWaterfall>
+      </Col>
+    </Row>
+    <Divider />
+    <Row>
+      <Col span={8}>
+      <TotalsTable></TotalsTable>
+      </Col>
+      <Col span={8}>
+      <EmbodiedCarbonPie></EmbodiedCarbonPie>
+      </Col>
+      <Col span={8}>
+      <EnergyCarbonPie></EnergyCarbonPie>
+      </Col>
+    </Row>
     </>
   );
   
