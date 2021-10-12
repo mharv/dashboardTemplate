b0d5bf56d3ab991a7d39c741d5b4aef64bb3da69
mitchell-harvey <mitchell.harvey@arup.com>
2021-10-12 14:49:30 +1100

    add options in main menu, create home view for entry

---
+++ src/components/HomeView.js
@@ -0,0 +1,98 @@
+import React, { Component, useEffect, useState } from 'react';
+import { Button, Col, Row, Select } from 'antd';
+import '../App.css';
+import { useIsAuthenticated, AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { SignInButton } from "./SignInButton";
+import { SignOutButton } from "./SignOutButton";
+import ProjectSelector  from "./ProjectSelector";
+import { loginRequest } from "../authConfig";
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
+
+const { Option } = Select;
+
+const HomeView = (props) => {
+
+    const [benchmarks, setBenchmarks] = useState([]);
+    const [selectedProject, setSelectedProject] = useState();
+
+    const url = 'https://sus-kpi-backend.azurewebsites.net';
+    // const token = props.token;
+    const urlComplete = url + '/projects?code=' + token;
+
+    const dispatch = useDispatch();
+
+    const getBau = () => {
+        axios.get(urlComplete).then(result => {
+            setBenchmarks(result.data);
+        })
+    }
+
+    useEffect(() => {
+        getBau();
+        // console.log(benchmarks)
+   }, []);
+
+    const isAuthenticated = useIsAuthenticated();
+    const { instance, accounts, inProgress } = useMsal();
+    const [accessToken, setAccessToken] = useState(null);
+
+    const selectChangeHandler = e => {
+        var result = benchmarks.filter(obj => {
+        return obj.Id == e.key
+        })
+        for (const property in result[0]) {
+        dispatch({type: 'update', key: [property], payload: result[0][property]})
+        }
+        dispatch({type: 'recalculate' })
+    }
+
+    const name = accounts[0] && accounts[0].name;
+
+    return (
+
+
+        <>
+        <Row>
+            <Col span={24}>
+                
+            <br />
+            <br />
+            <br />
+            {isAuthenticated ? <h5><center>Welcome to the Buildings Net Zero Carbon tool, {name}!</center></h5> : <SignInButton />}
+            </Col>
+        </Row>
+
+        <Row>
+        <Col span={24}>
+        <AuthenticatedTemplate>
+        {/* <>
+                {accessToken ?
+                    <p>Access Token Acquired! {accessToken}</p>
+                    :
+                    <Button variant="secondary" onClick={RequestAccessToken}>Request Access Token</Button>
+                }
+            </> */}
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Select style={{ width: 1000 }} 
+            labelInValue 
+            value={selectedProject}
+            placeholder={'Please select your project...'} 
+            onChange={selectChangeHandler}>
+            {benchmarks.map(option=> (<Option key={option.Id}>{option.JobNumber} - {option.ProjectTitle}</Option>))}
+            </Select>
+            </AuthenticatedTemplate>
+        </Col>
+        </Row>
+            
+        </>
+    );
+};
+
+
+export default HomeView;
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Menu } from 'antd';
-import { HomeOutlined, MailOutlined, DotChartOutlined, SettingOutlined, EditOutlined } from '@ant-design/icons';
+import { HomeOutlined, MailOutlined, DotChartOutlined, SettingOutlined, EditOutlined, SaveOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom"
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
 
 const centerStyle = {
   position: 'relative',
@@ -9,22 +11,74 @@ const centerStyle = {
   justifyContent: 'center'
 };
 
-class MainMenu extends React.Component {
-  state = {
+
+
+
+const MainMenu = (props) => {
+// class MainMenu extends React.Component {
+   let state = {
     current: 'mail',
   };
 
   
+  // const token = props.token;
+const url = 'https://sus-kpi-backend.azurewebsites.net';
+const urlComplete = url + '/projects?code=' + token;
 
-  handleClick = e => {
+  
+  const [current, setCurrent] = useState('home');
+
+  const saveButtonStyle = {
+    color: 'white',
+    backgroundColor: '#4BA046'
+  }
+
+  
+  const JobNumber = useSelector(state => state.JobNumber);
+  const SustainabilityChampionEmail = useSelector(state => state.SustainabilityChampionEmail);
+  const ProjectClimateChangeRiskReviewComplete = useSelector(state => state.ProjectClimateChangeRiskReviewComplete);
+  const MajorRisksNotMitigatedFor2050 = useSelector(state => state.MajorRisksNotMitigatedFor2050);
+  const MajorRisksNotMitigatedFor2070 = useSelector(state => state.MajorRisksNotMitigatedFor2070);
+  const ProjectTeamEngagement = useSelector(state => state.ProjectTeamEngagement);
+  const ProjectClientEngagementDecarbonisation = useSelector(state => state.ProjectClientEngagementDecarbonisation);
+  const ProjectClientEngagementResilience = useSelector(state => state.ProjectClientEngagementResilience);
+  const ProjectArupImpactCarbon = useSelector(state => state.ProjectArupImpactCarbon);
+  const ProjectArupImpactResilience = useSelector(state => state.ProjectArupImpactResilience);
+
+  const save = () => {
+    console.log("test save button")
+    const axiosConfig = {
+      headers: {
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Headers": "access-control-allow-origin, access-control-allow-headers"
+      }
+    };
+    const body = { 
+      JobNumber: JobNumber,
+      SustainabilityChampionEmail: SustainabilityChampionEmail,
+      ProjectClimateChangeRiskReviewComplete: ProjectClimateChangeRiskReviewComplete,
+      MajorRisksNotMitigatedFor2050: MajorRisksNotMitigatedFor2050,
+      MajorRisksNotMitigatedFor2070: MajorRisksNotMitigatedFor2070,
+      ProjectTeamEngagement: ProjectTeamEngagement,
+      ProjectClientEngagementDecarbonisation: ProjectClientEngagementDecarbonisation,
+      ProjectClientEngagementResilience: ProjectClientEngagementResilience,
+      ProjectArupImpactCarbon: ProjectArupImpactCarbon,
+      ProjectArupImpactResilience: ProjectArupImpactResilience
+     };
+    axios.patch(urlComplete, JSON.stringify(body), axiosConfig).then(result => {
+      console.log(result)
+  })
+  }  
+
+  const handleClick = e => {
     console.log('click ', e);
-    this.setState({ current: e.key });
+    setCurrent(e.key);
   };
 
-  render() {
-    const { current } = this.state;
+  // render() {
+    // const { current } = this.state;
     return (
-      <Menu style={centerStyle} theme="light" onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+      <Menu style={centerStyle} theme="light" onClick={handleClick} selectedKeys={current} mode="horizontal">
         
         <Menu.Item key="home" icon={<HomeOutlined />}>
           
@@ -32,18 +86,19 @@ class MainMenu extends React.Component {
           Home 
           </Link>
         </Menu.Item> 
-        <Menu.Item key="NZCinput" icon={<EditOutlined />}>
-          
-        <Link to="/NZCinput">
-          NZC Buildings
-        </Link>
-        </Menu.Item>
-        <Menu.Item disabled={true} key="KPIinput" icon={<EditOutlined />}>
+        <Menu.Item key="KPIinput" icon={<EditOutlined />}>
           
         <Link to="/KPIinput">
           KPIs
         </Link>
         </Menu.Item>
+        <Menu.Item disabled={true} key="NZCinput" icon={<EditOutlined />}>
+          
+        <Link  to="/NZCinput">
+          NZC Buildings
+        </Link>
+        </Menu.Item>
+        
         <Menu.Item disabled={true} key="compare" icon={<DotChartOutlined />}>
         <Link to="/compare">
           Compare
@@ -54,9 +109,14 @@ class MainMenu extends React.Component {
           Admin
           </Link>
         </Menu.Item>
+        <Menu.Item style={saveButtonStyle} onClick={save} key="save" icon={<SaveOutlined />}>
+        
+          <strong>Save</strong>
+        
+        </Menu.Item>
       </Menu>
     );
-  }
+  // }
 }
 
 export default MainMenu;
